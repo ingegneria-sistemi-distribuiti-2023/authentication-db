fb17f11b65289c5552cc7033b77936c1d83d1798
Danilo Leocata <danilo.leocata@gmail.com>
2022-12-30 15:46:22 +0100

    docker-compose and init schema and sample data

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # authentication-db
  
+After executing `docker-compose up`
+
+```
+docker exec -it authdb mysql -uadmin -p
+```
+
+password: `admin`
+
+To show available database:
+
+```
+use authdb;
+```
--- schema.sql
+++ schema.sql
@@ -17,9 +17,9 @@ CREATE TABLE IF NOT EXISTS `authdb`.`users` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
   `username` VARCHAR(45) NULL,
   `password` VARCHAR(45) NULL,
+  `enabled` TINYINT NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
-
 
 -- -----------------------------------------------------
 -- Table `authdb`.`balances`
